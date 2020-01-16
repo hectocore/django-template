dd2cc04a6cef0497ed055c1078424a25757873a6
Hector Corral <hectorcorral78@gmail.com>
2020-01-16 17:53:55 +0100

    Template rebuild on top of Django 3.0.2

--- {{cookiecutter.project_slug}}/project/settings/development.py
+++ {{cookiecutter.project_slug}}/project/settings/development.py
@@ -1,7 +1,8 @@
 # Quick-start development settings - unsuitable for production
-# https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 from .base import os, BASE_DIR
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "{{cookiecutter.secret_key}}"
@@ -13,7 +14,7 @@ ALLOWED_HOSTS = []
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -21,10 +22,3 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# User uploaded files - not suitable for production!
-# https://docs.djangoproject.com/en/2.2/howto/static-files/#serving-files-uploaded-by-a-user-during-development
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
